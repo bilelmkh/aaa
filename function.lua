9ffa241b6bf10290eb46d58778315780358279b7
bilelmkh <74114149+bilelmkh@users.noreply.github.com>
2021-01-23 17:14:28 +0100

    Update and rename tp7.py to function.lua

---
+++ function.lua
@@ -0,0 +1,29 @@
+ESX.Game.SpawnVehicle = function(modelName, coords, heading, cb)
+	local model = (type(modelName) == 'number' and modelName or GetHashKey(modelName))
+
+	Citizen.CreateThread(function()
+		ESX.Streaming.RequestModel(model)
+
+		local vehicle = CreateVehicle(model, coords.x, coords.y, coords.z, heading, true, false)
+		local networkId = NetworkGetNetworkIdFromEntity(vehicle)
+		local timeout = 0
+
+		SetNetworkIdCanMigrate(networkId, true)
+		SetEntityAsMissionEntity(vehicle, true, false)
+		SetVehicleHasBeenOwnedByPlayer(vehicle, true)
+		SetVehicleNeedsToBeHotwired(vehicle, false)
+		SetVehRadioStation(vehicle, 'OFF')
+		SetModelAsNoLongerNeeded(model)
+		RequestCollisionAtCoord(coords.x, coords.y, coords.z)
+
+		-- we can get stuck here if any of the axies are "invalid"
+		while not HasCollisionLoadedAroundEntity(vehicle) and timeout < 2000 do
+			Citizen.Wait(0)
+			timeout = timeout + 1
+		end
+
+		if cb then
+			cb(vehicle)
+		end
+	end)
+end
